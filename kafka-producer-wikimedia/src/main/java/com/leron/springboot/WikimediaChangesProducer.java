b358bfefad2e20b883403b7620f932f5db539a53
Leron Charles <lc443@njit.edu>
2024-01-05 19:40:44 -0500

    add dependencies for producers to send messages with event handlers

---
+++ kafka-producer-wikimedia/src/main/java/com/leron/springboot/WikimediaChangesProducer.java
@@ -0,0 +1,38 @@
+package com.leron.springboot;
+
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import com.launchdarkly.eventsource.background.BackgroundEventSource;
+import lombok.AllArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class WikimediaChangesProducer {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage() throws InterruptedException {
+        String topic = "wikimedia_recenchange";
+
+        log.info(String.format("Message sent"));
+
+        //to read real time stream data from wikimedia, we use event source
+        BackgroundEventHandler eventHandler = new WikimediaChangesHandler(kafkaTemplate, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        URI uriUrl = URI.create(url);
+        EventSource.Builder esBuilder = new EventSource.Builder(uriUrl);
+        BackgroundEventSource.Builder eventSource = new BackgroundEventSource.Builder(eventHandler,esBuilder);
+        BackgroundEventSource source = eventSource.build();
+        source.start();
+
+        TimeUnit.MINUTES.sleep(10);
+  }
+}
